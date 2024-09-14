c59c149456d8f5c39086c81777cf290047b95ccc
Ayush Singh <ayush.singh18@outlook.com>
2024-09-14 18:29:20 +0530

    Added second Model

--- EEG ML Model/app.py
+++ EEG ML Model/app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, jsonify, render_template
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_recall_fscore_support
 import pandas as pd
 import pickle
-import os
+import json
 
 app = Flask(__name__)
 
 # Load the Random Forest model
-rf_model = pickle.load(open('C:\\Users\\nisar\\one drive\\Desktop\\IEEE\\EEG ML Model\\model.pkl', 'rb'))
+rf_model = pickle.load(open(r'EEG ML Model/RF_model.pkl', 'rb'))
+lr_model = pickle.load(open(r'EEG ML Model/LR_model.pkl', 'rb'))
 
 # Serve the main HTML page
 @app.route('/')
@@ -14,30 +16,38 @@ def home():
     return render_template('index.html')
 
 # Handle the prediction route
-@app.route('/predict', methods=['POST'])
-def predict():
-    file = request.files['file']
+@app.route('/predict/<string:model_type>', methods=['POST'])
+def predict(model_type):
+    file = request.files['file'] # Uploaded file (.csv)
     
     if 'file' not in request.files:
         return jsonify({'error': 'No file uploaded'}), 400
 
     if file.filename == '':
         return jsonify({'error': 'No selected file'}), 400
-
+    
     try:
         # Read the uploaded CSV file
         eeg_data = pd.read_csv(file)
         
         # Assuming your model expects the same feature columns as training
         X = eeg_data.drop(['label', 'file_name'], axis=1)
+        Y = eeg_data['label']
 
-        # Make prediction
-        prediction = rf_model.predict(X)
+        if model_type == 'lr':
+            prediction = lr_model.predict(X)
+        elif model_type == 'rf':
+            prediction = rf_model.predict(X)
 
-        # Return the first prediction
-        result = int(prediction[0])
+        accuracy = accuracy_score(Y, prediction)
+        conf_matrix = confusion_matrix(Y, prediction)
+        # class_report = classification_report(Y, prediction)
+        metrics = precision_recall_fscore_support(Y, prediction, average='macro')
+        precision = metrics[0]
+        recall = metrics[1]
+        f1 = metrics[2]
 
-        return jsonify({'prediction': result})
+        return render_template('result.html', accuracy=accuracy, confusion_matrix=conf_matrix, precision=precision, recall=recall, f1_score=f1)
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
